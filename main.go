c6c6a25f14d8ad54d5eafc02ff3065d568ca9afa
David17c <devpelt@proton.me>
2026-05-26 21:01:27 +0200

    Improved comments in main

--- main.go
+++ main.go
@@ -9,21 +9,27 @@ import (
 )
 
 func main() {
+
+	// Read / create the config file
 	lines, asciicolor, headercolor, infocolor, labelcolor := config.ReadConfig()
 
+	// Collect the users system info
 	sys := model.CollectSystemInfo()
 
+	// Load and format the ascii art
 	asciiLines, asciicolor := assets.LoadASCII(
 		assets.LogoFS,
 		sys.ID,
 		asciicolor,
 	)
 
+	// Get the colors corresponding ascii codes
 	asciicolor = customization.GetColorCode(asciicolor)
 	headercolor = customization.GetColorCode(headercolor)
 	infocolor = customization.GetColorCode(infocolor)
 	labelcolor = customization.GetColorCode(labelcolor)
 
+	// Build the info lines
 	infoLines := render.BuildInfoLines(
 		sys,
 		lines,
@@ -32,5 +38,6 @@ func main() {
 		labelcolor,
 	)
 
+	// Put everything together and print it
 	render.PrintOutput(asciiLines, infoLines, asciicolor)
 }
